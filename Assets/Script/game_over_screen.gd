10b53eee2aa3c1126a7768469911a6e5b6e3eec6
Renzy20 <renzysteven1011@gmail.com>
2025-01-31 22:29:41 +0800

    All 100% Worked, Finished Debugging

--- Assets/Script/game_over_screen.gd
+++ Assets/Script/game_over_screen.gd
@@ -1,11 +1,15 @@
 extends Control
 
 func _on_restart_pressed() -> void:
-	get_node("/root/autoload").reset_lives()
-	get_tree().reload_current_scene()
+	var restart = Input.is_action_just_pressed("restart")
+	if restart == true:
+		get_node("/root/autoload").reset_lives()
+		get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	var quit = Input.is_action_just_pressed("quit")
+	if quit == true:
+		get_tree().quit()
 
 func set_score(value):
 	$Panel/Score.text = "Score: " + str(value)
